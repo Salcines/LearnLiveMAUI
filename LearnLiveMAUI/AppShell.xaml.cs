ccbab1e04129de6bc34798dc35eec11c152ab48d
Francisco Salcines <salcines@outlook.es>
2022-10-25 00:48:56 +0200

    Add project files.

---
+++ LearnLiveMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnLiveMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ LearnLiveMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LearnLiveMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
