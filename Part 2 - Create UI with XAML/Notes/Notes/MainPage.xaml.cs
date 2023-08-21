3535088ce08ec5171651e72c66c297a3d03ef68f
Francisco Salcines <Salcines@users.noreply.github.com>
2023-08-21 16:52:00 +0200

    Module 02 complete and merged (#5)
    * Initial App State
    
    * Migrate UI from code to XAML
    
    * Add makr-up font and background color and specific platform editor size

---
+++ Part 2 - Create UI with XAML/Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using Notes.Resources.Culture;
+
+namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+	private readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+	
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Editor.Text = (File.Exists(_fileName)) ? File.ReadAllText(_fileName) : string.Empty;
+	}
+
+	private void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, Editor.Text);
+	}
+
+	private void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		if (File.Exists(_fileName))
+		{
+			File.Delete(_fileName);
+		}
+
+		Editor.Text = string.Empty;
+	}
+}
+
---
+++ Part 2 - Create UI with XAML/Notes/README.md
@@ -0,0 +1,15 @@
+# Create a UI using XAML
+This solkution belogs to the second part of **.NET MAUI Learn Live** 
+In the initial state, we have a UI defined through C# code, and the objective is doble:
+
+1. Generate the same UI through XAML Page as we can see next.
+
+![Notes UI](Notes/Resources/Images/Notes_UI.png)
+
+2. Customize the color of the UI components like show the next image
+
+![App colors](Notes/Resources/Images/windows_android_app_colors.png)
+
+3. Customize the height of the editor control depending of the platform like show the next image
+
+![Editor screen](Notes/Resources/Images/windows_android_editor_screen.png)
