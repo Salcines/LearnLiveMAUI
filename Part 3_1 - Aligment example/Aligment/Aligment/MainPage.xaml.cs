89a0f36b2f7f2736f9f13edc9795c76a59b51ad9
Francisco Salcines <salcines@outlook.es>
2023-08-22 16:16:00 +0200

    Aligment layouts example

---
+++ Part 3_1 - Aligment example/Aligment/Aligment/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Aligment;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnHorizontalStartClicked(object sender, EventArgs e)
+		=> Target.HorizontalOptions = LayoutOptions.Start;
+
+	private void OnHorizontalCenterClicked(object sender, EventArgs e)
+	=> Target.HorizontalOptions = LayoutOptions.Center;
+
+	private void OnHorizontalEndClicked(object sender, EventArgs e)
+	=> Target.HorizontalOptions = LayoutOptions.End;
+
+	private void OnHorizontalFillClicked(object sender, EventArgs e)
+	=> Target.HorizontalOptions = LayoutOptions.Fill;
+
+	private void OnVerticalStartClicked(object sender, EventArgs e)
+	=> Target.VerticalOptions = LayoutOptions.Start;
+
+	private void OnVerticalCenterClicked(object sender, EventArgs e)
+		=> Target.VerticalOptions = LayoutOptions.Center;
+
+	private void OnVerticalEndClicked(object sender, EventArgs e)
+	=> Target.VerticalOptions = LayoutOptions.End;
+
+	private void OnVerticalFillClicked(object sender, EventArgs e)
+		=> Target.VerticalOptions = LayoutOptions.Fill;
+
+
+}
+
