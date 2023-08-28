e8f73137964a4603fb9f40b2793ee596fb30e5b2
Francisco Salcines <Salcines@users.noreply.github.com>
2023-08-28 16:34:03 +0200

    Module 03 Finish Exercise 3  (#7)
    * Aligment layouts example
    
    * solution created with default UI deleted
    
    * solution created with default UI deleted
    
    * App with the initial layout
    
    * Path restructure aligment example app
    
    * Improve the UI using StackLayout containers
    
    * Adap xaml to new emulator
    
    * Initial state TipCalculator usign gri layout
    
    * Path reestructure TipCalculator grid layout app
    
    * Add imagen 'after' to a readme document
    
    * Improve the UI usign Grid layout container

---
+++ Part 3_3 - Grid UI/TipCalculatorGrid/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace TipCalculatorGrid;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		BillInput.TextChanged += (s, e)
+			=> CalculateTip(false, false);
+		RoundDown.Clicked +=(sender, e)
+			=> CalculateTip(true, false);
+		RoundUp.Clicked += (s, e)
+			=> CalculateTip(false, true);
+
+		TipPercentageSlider.ValueChanged += (s, e) =>
+		{
+			double percentage = Math.Round(e.NewValue);
+			TipPercentage.Text = $"{percentage}%";
+			CalculateTip(false, false);
+		};
+	}
+
+	private void CalculateTip(bool roundDown, bool roundUp)
+	{
+		if (Double.TryParse(BillInput.Text, out double netBill) && netBill > 0)
+		{
+			double percentage = Math.Round(TipPercentageSlider.Value);
+			double tip = Math.Round(netBill * (percentage / 100.0), 2);
+
+			double totalBill = netBill + tip;
+
+			if (roundUp)
+			{
+				totalBill = Math.Ceiling(totalBill);
+			}
+			else if (roundDown)
+			{
+				totalBill = Math.Floor(totalBill);
+				tip = totalBill - netBill;
+			}
+
+			TipOutput.Text = tip.ToString("C");
+			TotalOutput.Text = totalBill.ToString("C");
+		}
+	}
+
+	private void OnNormalTip(object sender, EventArgs e)
+	=> TipPercentageSlider.Value = 15;
+
+	private void OnGenerousTip(object sender, EventArgs e)
+		=> TipPercentageSlider.Value = 20;
+
+}
+
