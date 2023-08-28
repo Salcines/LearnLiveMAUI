01a6e8eb25801091e4f1cd8edcf27f182c40ddac
Francisco Salcines <Salcines@users.noreply.github.com>
2023-08-28 16:31:01 +0200

    Module 03 - Complete exercises 1 and 2 (#6)
    * Aligment layouts example
    
    * solution created with default UI deleted
    
    * solution created with default UI deleted
    
    * App with the initial layout
    
    * Path restructure aligment example app
    
    * Improve the UI using StackLayout containers
    
    * Adap xaml to new emulator
    
    * Initial state TipCalculator usign gri layout
    
    * Path reestructure TipCalculator grid layout app
    
    * Add imagen 'after' to a readme document
    
    * Delete Grid solution due incomprensible errors

---
+++ Part 3_1 - Aligment example/README.md
@@ -0,0 +1,15 @@
+# Aligment Example
+
+A small app to see the effect of the four primary layout options such a horizontal as vertical option. This four options are:
+
+1. Start --> For horizontal aligment the control is align to the left hand side of the parent layout and for vertical aligment it possitions will be at top of the parent layout
+![start horizontally and vertically](Aligment/Resources/Images/start.png)
+
+2. Center --> For horizontal and vertical aligment, centers the control at parent layout
+![center horizontally and vertically](Aligment/Resources/Images/center.png)
+
+3. End --> For horizontal aligment, the control is align to the right hand side of the parent layout and for vertical aligment it possitions will be at bottom of the parent layout
+![end horizontally and vertically](Aligment/Resources/Images/end.png)
+
+4. Fill --> For horizontal aligment, the control fills the width of the parent layout and for vertical aligment it possitions fill the height of the parent layout
+![fill horizontally and vertically](Aligment/Resources/Images/fill.png)
---
+++ Part 3_2 - StackLayout UI/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace TipCalculator;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		BillInput.TextChanged += (s, e) => CalculateTip(false, false);
+		RoundDown.Clicked += (s, e) => CalculateTip(true, false);
+		RoundUp.Clicked += (s, e) => CalculateTip(false, true);
+
+		TipPercentSlider.ValueChanged += (s, e) =>
+		{
+			double percentage = Math.Round(e.NewValue);
+			TipPercent.Text = $"{percentage}%";
+			CalculateTip(false, false);
+		};
+	}
+
+	private void CalculateTip(bool roundDown, bool roundUp)
+	{
+		if (Double.TryParse(BillInput.Text, out var netBill) && netBill > 0)
+		{
+			double percentage = Math.Round(TipPercentSlider.Value);
+			double tip = Math.Round(netBill * (percentage / 100), 2);
+
+			double totalBill = netBill + tip;
+
+			if (roundUp)
+			{
+				totalBill = Math.Ceiling(totalBill);
+				tip = totalBill - netBill;
+			}
+			else if (roundDown)
+			{
+				totalBill = Math.Floor(totalBill);
+				tip = totalBill - netBill;
+			}
+
+			TipOutput.Text = tip.ToString("C");
+			TotalOutput.Text = totalBill.ToString("C");
+		}
+	}
+
+	private void OnNormalTip(object sender, EventArgs e)
+		=> TipPercentSlider.Value = 15;
+	
+
+	private void OnGenerousTip(object sender, EventArgs e)
+		=> TipPercentSlider.Value = 20;
+	
+}
+
