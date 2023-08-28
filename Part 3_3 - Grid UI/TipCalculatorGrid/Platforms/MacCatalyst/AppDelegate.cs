2ca26daf2ab9dc1a8c6e2665f4e3b80e19c44b84
Francisco Salcines <salcines@outlook.es>
2023-08-28 10:17:48 +0200

    Delete Grid solution due incomprensible errors

--- Part 3_3 - Grid UI/TipCalculatorGrid/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace TipCalculatorGrid;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
