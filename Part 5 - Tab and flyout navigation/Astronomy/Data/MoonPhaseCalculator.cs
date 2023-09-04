6f4a5050211f94de302dda411586b685444e6a83
Francisco Salcines <salcines@outlook.es>
2023-09-04 16:00:38 +0200

    MoonPhasePage created. Deploy error

---
+++ Part 5 - Tab and flyout navigation/Astronomy/Data/MoonPhaseCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy.Data
+{
+	public static class MoonPhaseCalculator
+    {
+	    public enum Phase
+	    {
+		    New,
+			WaxingCrescent,
+			FirstQuarter,
+			WaxingGibbous,
+			Full,
+			WaningGibbous,
+			LastQuarter,
+			WaningCrescent
+	    }
+
+	    private const double SynodicLength = 29.530588853;
+	    private static readonly DateTime ReferenceNewMoonDate = new DateTime(2017, 11, 18);
+
+	    public static Phase GetPhase(DateTime date)
+	    {
+		    return GetPhase(GetAge(date));
+	    }
+
+	    private static double GetAge(DateTime date)
+	    {
+		    double days = (date - ReferenceNewMoonDate).TotalDays;
+
+			return days % SynodicLength;
+	    }
+
+	    static Phase GetPhase(double age)
+	    {
+		    return age switch
+		    {
+			    < 1 => Phase.New,
+			    < 7 => Phase.WaxingCrescent,
+			    < 8 => Phase.FirstQuarter,
+			    < 14 => Phase.WaxingGibbous,
+				< 15 => Phase.Full,
+				< 22 => Phase.WaningGibbous,
+				< 23 => Phase.LastQuarter,
+				< 29 => Phase.WaningCrescent,
+			    _ => Phase.New
+		    };
+	    }
+    }
+}
---
+++ Part 5 - Tab and flyout navigation/Astronomy/Pages/MoonPhasePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Astronomy.Data;
+
+namespace Astronomy.Pages;
+
+public partial class MoonPhasePage : ContentPage
+{
+	public MoonPhasePage()
+	{
+		InitializeComponent();
+
+		InitializeUi();
+	}
+
+	private void InitializeUi()
+	{
+		var phase = MoonPhaseCalculator.GetPhase(DateTime.Now);
+
+		DateLabel.Text = DateTime.Today.ToString("D");
+		MoonPhaseIconLabel.Text = MoonPhaseEmojis[phase];
+		MoonPhaseTextLabel.Text = phase.ToString();
+
+		SetMoonPhaseLabels(PhaseIcon1Label, PhaseText1Label, 1);
+		SetMoonPhaseLabels(PhaseIcon2Label, PhaseText2Label, 2);
+		SetMoonPhaseLabels(PhaseIcon3Label, PhaseText3Label, 3);
+		SetMoonPhaseLabels(PhaseIcon4Label, PhaseText4Label, 4);
+	}
+
+	private void SetMoonPhaseLabels(Label labelIcon, Label labelText, int dayOffset)
+	{
+		var phase = MoonPhaseCalculator.GetPhase(DateTime.Now.AddDays(dayOffset));
+		labelIcon.Text = MoonPhaseEmojis[phase];
+		labelText.Text = DateTime.Now.AddDays(dayOffset).DayOfWeek.ToString();
+	}
+
+	private static readonly Dictionary<MoonPhaseCalculator.Phase, string> MoonPhaseEmojis =
+		new Dictionary<MoonPhaseCalculator.Phase, string>()
+		{
+			{ MoonPhaseCalculator.Phase.New, "🌑" },
+			{ MoonPhaseCalculator.Phase.WaningCrescent, "🌒" },
+			{ MoonPhaseCalculator.Phase.FirstQuarter, "🌓" },
+			{ MoonPhaseCalculator.Phase.WaxingGibbous, "🌔" },
+			{ MoonPhaseCalculator.Phase.Full, "🌕" },
+			{ MoonPhaseCalculator.Phase.WaningGibbous, "🌖" },
+			{ MoonPhaseCalculator.Phase.LastQuarter, "🌗" },
+			{ MoonPhaseCalculator.Phase.WaningCrescent, "🌘" }
+		};
+}
